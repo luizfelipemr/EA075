4469ae66824c4fcb445b14c1f0df1a2f8f88f353
luizfelipemr <luizfmrego@gmail.com>
2017-09-09 23:28:21 -0300

    Range controlled with function map

--- sensor/XXXX/XXXX.ino
+++ sensor/XXXX/XXXX.ino
@@ -3,45 +3,77 @@
 
  */
  
- //Inicializa pino para leituras do sensor
- int spin = 3;
+ //Inicialize reading variable
+ int sensor = A0;
+ 
+ //Variable declaration which place sensor values
+ int valor;
+
 
 void setup() {
-  // Inicializa comunicaçao serial
+  // Inicialize serial comunication
   Serial.begin(9600);
   
-  //Leitura dos dados do sensor
-  pinMode(spin, INPUT);
-  
-  // Inicializar pino digital 13 como saida
-  pinMode(13, OUTPUT);
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
   
 }
 
-void loop() {
-  //Declaraçao de variavel que alocara valores do sensor XXXXX
-  int sensor;
+void loop() {  
 
-  //Leitura dos valores do sensor XXXXX
-  sensor = pulseIn(spin, HIGH);
-  Max_sensor = 0;
-  Min_sensor = 0:
+  //Reads the sensor values
+  valor = analogRead(sensor);
+  
+  Serial.print("Valor:");
+  Serial.println(valor);
   
 
-  //Converte os dados do sensor para controle do LED
-  //Valores convertidos para range entre 100 e 1000
+  /*Converte os dados do sensor para controle do LED
+  /Valores convertidos para range entre 100 e 1000
   int value;
   int aux;
   aux = 1000(sensor-Min_sensor)-100(sensor-Max_sensor);
   value = aux/(Max_sensor-Min_sensor);
+  */
   
+  //Implementation of raange control
+  int distancia = map(valor,0,1023,0,2);
   
-  //Acende e apaga o LED com tempo controlado pelo sensor
-  digitalWrite(13, HIGH);   
-  delay(value);              
-  digitalWrite(13, LOW);    
-  delay(value);         
+  //3 possible states
+  switch (distancia) {
 
-  delay(50);  
+case 0:
+
+digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+
+break;
+
+case 1:
+
+digitalWrite(LED_BUILTIN, HIGH);
+  delay(750);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(750);
+
+break;
+
+case 2:
+
+digitalWrite(LED_BUILTIN, HIGH);
+  delay(350);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(350);
+
+break;
+
+default:
+  digitalWrite(LED_BUILTIN, LOW);
+
+break;
   
+}
 }
